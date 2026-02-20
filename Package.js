4a64c3ffee6af359ea587e9f6a21df68a577bfce
ilqcks3nce <candacesegula@gmail.com>
2026-02-20 13:40:53 +0000

    Create Package.js

---
+++ Package.js
@@ -0,0 +1,11 @@
+{
+  "name": "big-brother-bot",
+  "version": "1.0.0",
+  "main": "index.js",
+  "scripts": { "start": "node index.js" },
+  "dependencies": {
+    "discord.js": "^14.13.0",
+    "dotenv": "^16.3.1",
+    "mongoose": "^8.0.0"
+  }
+}
